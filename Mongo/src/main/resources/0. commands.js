9e17b6a1ead3a25bac68f9456e4adbad5b3f3c41
ilnur <serbaevilnur@gmail.com>
2017-07-11 11:09:21 +0500

    mongo - day 2 in progress

--- Mongo/src/main/resources/0. commands.js
+++ Mongo/src/main/resources/0. commands.js
@@ -181,3 +181,38 @@ db.towns.find( {
 
 //DZ 1
 db.towns.find({ name : { $regex : /^new/, $options:"$i" } });
+
+//DAY 2
+
+function populatePhones(area,start,stop) {
+    for(var i=start; i < stop; i++) {
+        var country = 1 + ((Math.random() * 8) << 0);
+        var num = (country * 1e10) + (area * 1e7) + i;
+        db.phones.insert({
+            _id: num,
+            components: {
+                country: country,
+                area: area,
+                prefix: (i * 1e-4) << 0,
+                number: i
+            },
+            display: "+" + country + " " + area + "-" + i
+        });
+    }
+}
+
+populatePhones( 800, 5550000, 5650000 );
+
+db.phones.find().limit(2);
+
+db.phones.getIndexes();
+db.phones.find({display: "+1 800-5650001"}).explain("executionStats");  //54 millis
+db.phones.ensureIndex({display : 1}, {unique : true, dropDups : true});
+db.phones.find({display: "+1 800-5650001"}).explain("executionStats");  //2 millis after indexing
+
+db.setProfilingLevel(2);
+db.phones.find({ display : "+1 800-5650001" });
+db.system.profile.find();
+
+db.phones.ensureIndex({ "components.area" : 1}, { background : 1 });
+db.phones.getIndexes();
